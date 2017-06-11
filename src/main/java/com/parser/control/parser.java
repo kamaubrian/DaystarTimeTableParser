9e0021e65f727e6d5cecc0dc03f43f14975a89cb
mtotodev05 <mtotodev05@gmail.com>
2017-06-11 16:52:14 +0300

    Read xlxs

--- src/main/java/com/parser/control/parser.java
+++ src/main/java/com/parser/control/parser.java
@@ -1,7 +1,33 @@
 package com.parser.control;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.File;
+import java.io.FileInputStream;
+
 /**
  * Created by brian-kamau on 6/10/17.
  */
 public class parser {
+
+
+    private String file;
+    private String school;
+    private String unit;
+    private XSSFWorkbook table;
+    private XSSFSheet Sheet;
+
+    public parser(String file, String school,String unit) {
+        this.file = file;
+        this.school = school;
+        this.unit=unit;
+    }
+        public void parse() throws Exception{
+            System.out.println("Opening File:: "+file);
+            FileInputStream str = new FileInputStream(new File(file));
+            table = new XSSFWorkbook(str);
+            int sheets = table.getNumberOfSheets();
+            System.out.println("Sheets:"+sheets);
+        }
+
 }
--- src/main/java/com/parser/main.java
+++ src/main/java/com/parser/main.java
@@ -1,7 +1,39 @@
 package com.parser;
-
+import java.util.Scanner;
+import com.parser.control.parser;
 /**
  * Created by brian-kamau on 6/10/17.
  */
 public class main {
+    public main(){
+        super();
+    }
+
+
+    public static void main(String []args){
+        String path;
+        String unit;
+        String school; //Sheet Either Athi-River/ Nairobi.
+
+        System.out.println("***************\nTimeTableParser\n***************");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Path to Excel Timetable File");
+        path=scanner.nextLine();
+        System.out.println("Enter Unit Name");
+        unit=scanner.nextLine();
+        System.out.println("Enter Campus[Athi Or Nairobi]");
+        school=scanner.nextLine();
+
+        try{
+            parser par = new parser(path,unit,school);
+            par.parse();
+
+        }catch(Exception ex){
+            System.out.println(ex.getClass());
+            ex.printStackTrace();
+        }
+
+    }
+
+
 }
